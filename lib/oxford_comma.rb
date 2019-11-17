2a2b87a6911cb905585d7a1dd2117e742ac52875
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-17 17:15:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,2 @@
-def "#oxford_comma" do
-  it 'returns a string without any additional fomatting when given a 1-element array' do
-    expect(oxford_comma(["kiwi"])).to eq("kiwi")
-  end
+def "oxford_comma".split(",")
+end
